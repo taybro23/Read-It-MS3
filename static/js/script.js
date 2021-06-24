5eed14880f0a8a9902d5c260708a0516be896bd7
taybro23 <taybro343@outlook.com>
2021-06-24 19:12:42 +0000

    Button hover add/remove class added.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
+    $('.btn-large').hover(
+        function(){
+            $(this).removeClass("green", "accent-4")
+        },
+        function(){
+            $(this).addClass("green", "accent-4")
+        }
+    )
   });
