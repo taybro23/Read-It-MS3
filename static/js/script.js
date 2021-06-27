7e84eb5dcd99542d698234c9e92d309119348c5b
taybro23 <taybro343@outlook.com>
2021-06-27 12:16:12 +0000

    Added optional review text area and initialization.

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
+    $('input#input_text, textarea#review').characterCounter();
     $('.btn-large').hover(
         function(){
             $(this).removeClass("green", "accent-4")
