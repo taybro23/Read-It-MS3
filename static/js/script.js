3e6dfb8d5b2beaf359bcc432ecaa33288edef9b7
taybro23 <taybro343@outlook.com>
2021-07-09 17:02:29 +0000

    Changed class of hover function.

--- static/js/script.js
+++ static/js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
     $('input#input_text, textarea#review').characterCounter();
-    $('.btn-large').hover(
+    $('.grn-btn').hover(
         function(){
             $(this).removeClass("green", "accent-4")
         },
         function(){
             $(this).addClass("green", "accent-4")
         }
-    )
+    );
     
     validateMaterializeSelect();
     function validateMaterializeSelect() {
